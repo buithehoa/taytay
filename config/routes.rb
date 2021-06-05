245cc5c02caf4caf8c86eeab056541ad7e716def
Bui The Hoa <buithehoa@gmail.com>
2021-06-05 16:19:04 +0700

    Install and configure Rubocop.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  root to: 'auth#index'
+
+  get 'auth/index'
+
   namespace :auth do
     get 'spotify/callback'
   end
-  get 'auth/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
