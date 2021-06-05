c25e69a6f63b51197c39f7cdea984309012811a9
Bui The Hoa <buithehoa@gmail.com>
2021-06-05 17:22:54 +0700

    Authenticate with Spotify.

--- app/controllers/auth/spotify_controller.rb
+++ app/controllers/auth/spotify_controller.rb
@@ -1,4 +1,8 @@
 class Auth::SpotifyController < ApplicationController
   def callback
+    spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
+
+    puts spotify_user.display_name
+    puts spotify_user.inspect
   end
 end
