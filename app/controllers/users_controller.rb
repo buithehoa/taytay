2b3f3e09a79e2bd69de315d433558fdeea491812
Bui The Hoa <buithehoa@gmail.com>
2023-10-26 13:36:21 +0700

    Remove all.

--- app/controllers/users_controller.rb
+++
@@ -1,7 +0,0 @@
-class UsersController < ApplicationController
-  def spotify
-    spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
-
-    puts spotify_user.inspect
-  end
-end
--- config/initializers/omniauth.rb
+++
@@ -1,8 +0,0 @@
-require 'rspotify/oauth'
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify,
-           '2fab4ba642b846619622f92328704db3',
-           '9873f5b2155948ab90feab471ceba287',
-           scope: 'user-read-email playlist-modify-public user-library-read user-library-modify'
-end
